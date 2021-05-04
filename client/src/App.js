0c8ff95729964e4f83111a59a88439d6ce963d9c
Veer Mehra <vmehar13@gmail.com>
2021-05-05 00:16:29 +0530

    signup functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import './Stylesheet/App.css';
 import Home from './Components/Home';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Signup from './Components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router className="App">
+      <Switch>
+        <Route path="/user-signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
